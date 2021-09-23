daef10b3d9f62919b4c9df81cc9835159d127e6b
STEMwithMrDuda <91220058+STEMwithMrDuda@users.noreply.github.com>
2021-09-23 13:11:06 -0500

    Added music (need to change it to Zippity Duda)

--- Power_Switch_for_AC/Updated_AC_Code_2.ino
+++ Power_Switch_for_AC/Updated_AC_Code_2.ino
@@ -1,3 +1,12 @@
+/*  Duda.AC.v3.0
+
+  This is a program to control a GE window air conditioning unit utlizing the built in pushbuttons 
+  (connections scratched on the board to disconnect).  It will control power, compressor, and fan.
+  Eventually it may integrate with thermostat.  For now it's ON or OFF.
+
+*/
+
+#include "pitches.h"
 // constants won't change. They're used here to set pin numbers:
 const int powerButton = 1; 
 const int modeButton = 3;  // can choose the fan speed
@@ -7,6 +16,11 @@ const int highSpeed = 6;  //  Turns the fan on high
 const int speakerPin = 7;  //  The piezo buzzer is connected to this
 const int compressorPin = 8;  //  This pin is connected to the relay the controls the compressor
 const int thermostatPin = 9;  //To be used in the future when I read the thermostate via serial.
+
+int shaveMelody[] = { NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4 }; //Shave and a haircut melody
+int shaveDurations[] = { 4, 8, 8, 4, 4, 4, 4, 4 }; // Shave and a Haircut note length
+int zippityDuda[] = { NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_A5, NOTE_F4, NOTE_F4, NOTE_D4, NOTE_E4, NOTE_E4 };  //  Zippity Duda melody
+int zippityDurations[] = { 4, 8, 8, 4, 2, 4, 8, 8, 4, 2 }; //  Zippity Duda note length
 
 // variables will change:
 int stateCompressorPin = LOW;     // the current state of LED
@@ -32,6 +46,16 @@ void setup() {
   pinMode(speakerPin, OUTPUT);
   pinMode(thermostatPin, INPUT);
   statePowerButton = LOW;
+  
+  // plays music
+    {
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    int shaveDuration = 1000 / shaveDurations[thisNote];
+    tone(8, shaveMelody[thisNote], shaveDuration);
+    int pauseBetweenNotes = shaveDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
 }
 
 void loop() {
